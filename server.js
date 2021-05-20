b508cf3cba60ad8cac0f52024bb98e4370e25675
Jordi <jordifranssenmij@gmail.com>
2021-05-20 10:18:03 +0200

    Loginsysteem gebouwd

---
+++ server.js
@@ -0,0 +1,116 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const axios = require('axios')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    _id => users.find(user => user._id === _id)
+)
+
+axios.get('http://jordi.dsmynas.com:3000/users')
+    .then(result => {
+        users = result.data
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {
+        name: req.user.name
+    })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        axios.post('http://jordi.dsmynas.com:3000/users', {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    axios.get('http://jordi.dsmynas.com:3000/users')
+        .then(result => {
+            users = result.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    axios.get('http://jordi.dsmynas.com:3000/users')
+        .then(result => {
+            users = result.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+
+    next()
+}
+
+
+
+app.listen(3001)
